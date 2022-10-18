6eb1cf1cb77054589b98c245282e249605bacbbf
Debo <deborahkollman@gmail.com>
2022-10-18 16:16:44 -0300

    first commit

---
+++ api/src/app.ts
@@ -0,0 +1,42 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+import config from '../lib/config';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import routes from '../routes/index';
+
+const app: Application = express();
+app.use(express.urlencoded({extended: true, limit: '50mb'})); //middleware
+app.use(express.json({limit: '50mb'}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(
+  cors({
+    origin: config.cors,
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  })
+);
+
+interface error {
+  status: number;
+  message: string;
+}
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hola typescript!');
+});
+
+app.use('/api', routes);
+
+app.use((err: error, req: Request, res: Response, next: NextFunction) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+export default app;
