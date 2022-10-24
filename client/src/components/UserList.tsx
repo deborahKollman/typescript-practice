c08bc301ec378920fdd3f47622f858a6cbb4957a
Debo <deborahkollman@gmail.com>
2022-10-24 16:44:26 -0300

    style: add list style and loading style

---
+++ client/src/components/UserList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { User } from "../redux/actions";
+import { Avatar, List } from 'antd';
+
+type AppProps = {
+    data: User[]
+}
+
+export default function UserList({data}: AppProps){
+    return(
+    <List
+        itemLayout="horizontal"
+        bordered={true}
+        size="large"
+        dataSource={data}
+        renderItem={item => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar src="https://bitsofco.de/content/images/2018/12/Screenshot-2018-12-16-at-21.06.29.png" />}
+              title={<a href="#">{item.name} {item.lastName}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      />)
+}
--- client/src/components/Users.tsx
+++ client/src/components/Users.tsx
@@ -2,6 +2,8 @@ import React,{useEffect, useState} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
 import {User, fetchUsers} from '../redux/actions';
 import { AppDispatch } from '../redux/store';
+import { Spin } from 'antd';
+import UserList from './UserList';
 
 
 export default function Users() {
@@ -11,21 +13,18 @@ export default function Users() {
     const users=useSelector((state:User[])=>state)
     
     useEffect(() => {
-        setLoading(true)
         dispatch(fetchUsers())
         setLoading(false)
     }, []);
 
     
     return (
-        <div>
+        <>
             {!isLoading?
             <div>
-            {users?.map((user: User) => {
-                return (<div key={user.id}>{user.id}) {user.name} {user.lastName}</div>
-                );
-            })}
-            </div>:<div><h3>Loading</h3></div>}
-        </div>
+                <h1>Users</h1>
+                {users?<UserList data={users}/>:null}
+            </div>:<Spin />}
+        </>
     );
 }
