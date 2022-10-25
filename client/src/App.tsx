106d6e591439b402b8ab351e16aab4c1a876a485
Debo <deborahkollman@gmail.com>
2022-10-25 16:30:43 -0300

    feat: add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Users from './components/Users';
+import UserCreateForm from './components/UserCreateForm';
+import path from 'path';
 
 function App() {
   return (
     <div className="App">
-      <Users/>
+      <Routes>
+        <Route element={<Users/>} path='/'/>
+        <Route element={<UserCreateForm/>} path='/create'/>
+      </Routes>
     </div>
   );
 }
