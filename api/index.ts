62fc2bf260dbfcbf4212924b37af42977d3f0cd4
Debo <deborahkollman@gmail.com>
2022-11-02 17:08:29 -0300

    change: changed database connection to mongo

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,24 @@
-import {sequelize} from './src/db';
+// import {sequelize} from './src/db';
 import app from './src/app';
-sequelize
- .sync({force: false, logging: false})
- .then(() => {
-  console.log('base de datos conectada! :D');
-  app.listen(3001, function () {
-   console.log('App is listening on port 3001!');
-  });
- })
- .catch((err) => console.error(err));
+import mongoose from 'mongoose';
+import config from './lib/config';
+
+mongoose
+  .connect(`mongodb+srv://deboKollman:${config.mongo_password}@cluster0.o636h0f.mongodb.net/${config.mongo_db}?retryWrites=true&w=majority`)
+  .then(() =>
+    app.listen(config.port, () =>
+      console.log(`Server running on http://localhost:${config.port}`)
+    )
+  )
+  .catch(error => {
+    throw error
+  })
+// sequelize
+//  .sync({force: false, logging: false})
+//  .then(() => {
+//   console.log('base de datos conectada! :D');
+//   app.listen(3001, function () {
+//    console.log('App is listening on port 3001!');
+//   });
+//  })
+//  .catch((err) => console.error(err));
--- api/lib/config.ts
+++ api/lib/config.ts
@@ -3,15 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
- dbUser: process.env.DB_USER || 'postgres',
- dbPassword: process.env.DB_PASSWORD || '1234',
- dbHost: process.env.DB_HOST || 'localhost',
- dbName: process.env.DB_NAME || 'workshop',
- dbPort: process.env.DB_PORT || '5432',
+//  dbUser: process.env.DB_USER || 'postgres',
+//  dbPassword: process.env.DB_PASSWORD || '1234',
+//  dbHost: process.env.DB_HOST || 'localhost',
+//  dbName: process.env.DB_NAME || 'workshop',
+//  dbPort: process.env.DB_PORT || '5432',
  dev: process.env.NODE_ENV !== 'production',
  port: process.env.API_PORT || '3001',
  host: process.env.API_host || 'localhost',
  cors: process.env.CORS || '*',
+ mongo_user: process.env.MONGO_USER,
+ mongo_password: process.env.MONGO_PASSWORD,
+ mongo_db: process.env.MONGO_DB
 };
 
 export default config;
