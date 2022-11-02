0d7b30ede270bfbf5552ceaaf0356549fa8f19f2
Debo <deborahkollman@gmail.com>
2022-11-02 16:06:47 -0300

    change: changing database to mongoDB, add mongoose and user model

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,22 +1,58 @@
-import {Model, Column, Table, CreatedAt, UpdatedAt, PrimaryKey, AutoIncrement} from 'sequelize-typescript';
-@Table
-export class User extends Model<User> {
-    @PrimaryKey
-    @AutoIncrement
-    @Column
-    id!: number;
+//MongoDB
 
-    @Column
-    name!: string;
+import { Document, model, Schema } from "mongoose";
 
-    @Column
-    lastName!: string;
-
-    @CreatedAt
-    @Column
-    createdAt!: Date;
-
-    @UpdatedAt
-    @Column
-    updatedAt!: Date;
+export interface IUser extends Document{
+    id: number,
+    name: string,
+    lastName: string
 }
+
+const userSchema: Schema = new Schema(
+    {
+        id:{
+            type: Number,
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+  )
+  
+export default model<IUser>("User", userSchema)
+
+
+//PostgreSQL
+// import {Model, Column, Table, CreatedAt, UpdatedAt, PrimaryKey, AutoIncrement} from 'sequelize-typescript';
+// @Table
+// export class User extends Model<User> {
+//     @PrimaryKey
+//     @AutoIncrement
+//     @Column
+//     id!: number;
+
+//     @Column
+//     name!: string;
+
+//     @Column
+//     lastName!: string;
+
+//     @CreatedAt
+//     @Column
+//     createdAt!: Date;
+
+//     @UpdatedAt
+//     @Column
+//     updatedAt!: Date;
+// }
+
+
