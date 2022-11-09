a4625862aa50e6023afd0b47364ce01f0d6106a7
Debo <deborahkollman@gmail.com>
2022-11-09 14:10:08 -0300

    fix:redux reducer initial state

--- client/src/redux/reducer.ts
+++ client/src/redux/reducer.ts
@@ -1,19 +1,13 @@
 import { ActionTypes, User, FetchUsersAction } from './actions';
 
-
-export interface stateI {
-    users: User[];
-}
    
-const initialState: stateI = {
-    users:[],
-};
+const initialState: User[] = [];
 
 export interface StoreState {
     users: User[];
 }
    
-const reducer = (state: User[] = [], action: FetchUsersAction) => {
+const reducer = (state: User[] = initialState, action: FetchUsersAction) => {
     switch (action.type) {
         case ActionTypes.fetchUsers: return action.payload;
         default: return state;
